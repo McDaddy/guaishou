ac29c8250a0c4f782d1bb6e7273629a19d780be0
Peter Wang <happypeter1983@gmail.com>
2015-04-09 15:25:25 +0800

    i

--- tools/convert.rb
+++ tools/convert.rb
@@ -5,6 +5,9 @@ File.open("cards.md", 'r') do |f|
   str = f.read
 end
 
+
+# 上面的 gsub 先执行，执行完了就交给下一行做替换，替换后的内容再交给下一行的 gsub，顺序相关的
+
 str = str.gsub(/^###\s?(.*?)\s?$/, "<h3>\\1</h3>\n").
   gsub(/^任务：\s?(.*?)\n$/, "<div class=\"card\">\n<h3>\\1</h3>\n<ul>").
   gsub(/^\-\s+baidu:\s?(.*?)$/, "</ul>\n<a href='\\1'>视频</a>\n</div>").

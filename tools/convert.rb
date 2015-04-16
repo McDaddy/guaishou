00b86dce30bd19ca17a2b1c68241233772ff4638
Peter Wang <happypeter1983@gmail.com>
2015-04-16 22:30:55 +0800

    i

--- tools/convert.rb
+++ tools/convert.rb
@@ -32,7 +32,7 @@ File.open("index.md", 'a') do |f|
 end
 
 
-system 'git add .&&git commit -m"i"&&git push'
+system 'git add -A&&git commit -m"i"&&git push'
 system 'echo "deploy new stuff"'
 system 'ssh peter@happyec.org "source .profile &&/home/peter/bin/ggg.sh"'
 
